98057168eacbd17e9a7ed56bb8f01e55362697fe
gliderkite <gliderkite@gmail.com>
2019-02-11 19:49:35 +0000

    implement array nodes comparison in depth

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use serde_json::Value;
 
+use std::cmp;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
@@ -44,34 +45,57 @@ fn read_from_file<P: AsRef<Path>>(path: P) -> Result<Value, Box<Error>> {
 }
 
 #[derive(Debug)]
-enum NodeDiff<'a> {
+enum Delta<'a> {
     Equal(&'a Value),
     DifferentContent((&'a Value, &'a Value)),
     DifferentVariant((&'a Value, &'a Value)),
     MissingInSecond(&'a Value),
     MissingInFirst(&'a Value),
-    Node(HashMap<&'a String, NodeDiff<'a>>),
+    List(Vec<Delta<'a>>),
+    Map(HashMap<&'a String, Delta<'a>>),
 }
 
-impl<'a> NodeDiff<'a> {
-    fn new(lhs: &'a Value, rhs: &'a Value) -> NodeDiff<'a> {
+impl<'a> Delta<'a> {
+    fn new(lhs: &'a Value, rhs: &'a Value) -> Delta<'a> {
         if lhs == rhs {
-            NodeDiff::Equal(lhs)
+            Delta::Equal(lhs)
         } else {
-            NodeDiff::DifferentContent((lhs, rhs))
+            Delta::DifferentContent((lhs, rhs))
         }
     }
 }
 
 /// Compare two JSON nodes.
-fn compare<'a>(val1: &'a Value, val2: &'a Value) -> NodeDiff<'a> {
+fn compare<'a>(val1: &'a Value, val2: &'a Value) -> Delta<'a> {
     match (val1, val2) {
-        (Value::Null, Value::Null) => NodeDiff::Equal(val1),
-        (Value::Bool(_), Value::Bool(_)) => NodeDiff::new(val1, val2),
-        (Value::Number(_), Value::Number(_)) => NodeDiff::new(val1, val2),
-        (Value::String(_), Value::String(_)) => NodeDiff::new(val1, val2),
-        (Value::Array(_), Value::Array(_)) => NodeDiff::new(val1, val2),
+        (Value::Null, Value::Null) => Delta::Equal(val1),
+        (Value::Bool(_), Value::Bool(_)) => Delta::new(val1, val2),
+        (Value::Number(_), Value::Number(_)) => Delta::new(val1, val2),
+        (Value::String(_), Value::String(_)) => Delta::new(val1, val2),
+        (Value::Array(ref v1), Value::Array(ref v2)) => {
+            // compare according to the index of the nodes in the array
+            let mut v = Vec::with_capacity(cmp::max(v1.len(), v2.len()));
+            for (val1, val2) in v1.iter().zip(v2.iter()) {
+                let diff = compare(val1, val2);
+                v.push(diff);
+            }
+            let missing_in_second = v1.len() > v2.len();
+            let it = if missing_in_second {
+                v1.iter().skip(v2.len())
+            } else {
+                v2.iter().skip(v1.len())
+            };
+            for val in it {
+                if missing_in_second {
+                    v.push(Delta::MissingInSecond(val));
+                } else {
+                    v.push(Delta::MissingInFirst(val));
+                }
+            }
+            Delta::List(v)
+        }
         (Value::Object(ref m1), Value::Object(ref m2)) => {
+            // compare according to the key of the nodes in the map
             let mut nodes = HashMap::new();
             // iterate over the nodes of the first document
             for (k, v1) in m1.iter() {
@@ -80,18 +104,18 @@ fn compare<'a>(val1: &'a Value, val2: &'a Value) -> NodeDiff<'a> {
                     let diff = compare(v1, v2.unwrap());
                     nodes.insert(k, diff);
                 } else {
-                    nodes.insert(k, NodeDiff::MissingInSecond(v1));
+                    nodes.insert(k, Delta::MissingInSecond(v1));
                 }
             }
             // iterate over the nodes of the second document
             for (k, v2) in m2.iter() {
                 if !m1.contains_key(k) {
-                    nodes.insert(k, NodeDiff::MissingInFirst(v2));
+                    nodes.insert(k, Delta::MissingInFirst(v2));
                 }
             }
-            NodeDiff::Node(nodes)
+            Delta::Map(nodes)
         }
-        _ => NodeDiff::DifferentVariant((val1, val2)),
+        _ => Delta::DifferentVariant((val1, val2)),
     }
 }
 
@@ -110,6 +134,14 @@ mod tests {
         assert!(read_from_file("test/data/basic_variants_diff_1e.json").is_ok());
         assert!(read_from_file("test/data/basic_variants_diff_1f.json").is_ok());
         assert!(read_from_file("test/data/basic_variants_diff_1g.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_eq_1a.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_eq_1b.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_eq_2a.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_eq_2b.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_diff_1a.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_diff_1b.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_diff_2a.json").is_ok());
+        assert!(read_from_file("test/data/nested_variants_diff_2b.json").is_ok());
     }
 
     #[test]
